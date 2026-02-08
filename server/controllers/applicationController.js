2d877df3430c101e8b49a3a17ca61b28abb1ebf8
Jhankrit Sharma <sharmajhankrit@gmail.com>
2026-02-08 16:46:17 +0530

    application routes and application controller created

--- server/controllers/applicationController.js
+++ server/controllers/applicationController.js
@@ -29,4 +29,85 @@ const applyToJob = async (req, res) => {
     }
 };
 
-module.exports = applyToJob;
+const getMyApplications = async (req, res) => {
+    const candidateId = req.user.id;
+
+    try {
+        const result = await pool.query(`SELECT
+                                            a.id,
+                                            a.status,
+                                            a.created_at,
+                                            j.id AS job_id,
+                                            j.title,
+                                            j.location,
+                                            c.name AS company_name
+                                        FROM applications a
+                                        JOIN jobs j ON a.job_id = j.id
+                                        JOIN companies c ON j.company_id = c.id
+                                        WHERE a.candidate_id = $1
+                                        ORDER BY a.created_at DESC;`, [candidateId]);
+        return res.status(200).json({
+            count: result.rows.length,
+            data: result.rows,
+        });
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+const getJobApplications = async (req, res) => {
+    const { jobId } = req.params;
+    const ownerId = req.user.id;
+    
+    try {
+        const result = await pool.query(`SELECT
+                                            a.id,
+                                            a.status,
+                                            a.created_at,
+                                            a.cover_letter,
+                                            a.resume_url,
+                                            u.id AS candidate_id,
+                                            u.email AS candidate_email
+                                        FROM applications a
+                                        JOIN jobs j ON a.job_id = j.id
+                                        JOIN companies c ON j.company_id = c.id
+                                        JOIN users u ON a.candidate_id = u.id
+                                        WHERE j.id = $1
+                                            AND c.owner_id = $2
+                                        ORDER BY a.created_at DESC`,[jobId, ownerId]);
+        return res.status(200).json({
+            count: result.rows.length,
+            data: result.rows,
+        });
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+const updateApplication = async (req, res) => {
+    const applicationId = req.params.id;
+    const ownerId = req.user.id;
+    const { status } = req.body;
+    if (!status) {
+        return res.status(400).json({ error: 'Status is required' });
+    }
+    try {
+        const check = await pool.query(`SELECT a.id
+                                        FROM applications a
+                                        JOIN jobs j ON a.job_id = j.id
+                                        JOIN companies c ON j.company_id = c.id
+                                        WHERE a.id = $1 AND c.owner_id = $2`, [applicationId, ownerId]);
+        if (check.rows.length === 0) {
+            return res.status(404).json({ error: 'Application not found or unauthorized', });
+        }
+        const result = await pool.query(`UPDATE applications SET status = $1 WHERE id = $2 RETURNING id, status`, [status, applicationId]);
+        return res.status(200).json({ data: result.rows[0], });
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+module.exports = { applyToJob, getMyApplications, getJobApplications, updateApplication };
--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const requireRole = require('../middleware/requireRole');
 const requireAuth = require('../middleware/requireAuth');
-const applyToJob = require('../controllers/applicationController');
+const { applyToJob, getMyApplications, getJobApplications, updateApplication } = require('../controllers/applicationController');
 
 const router = express.Router();
 
 router.post("/", requireAuth, requireRole("CANDIDATE"), applyToJob);
 
-//router.get("/me", requireAuth, requireRole("CANDIDATE"));
+router.get("/me", requireAuth, requireRole("CANDIDATE"), getMyApplications);
 
-//router.get("/job/:jobId", requireAuth, requireRole("COMPANY"));
+router.get("/job/:jobId", requireAuth, requireRole("COMPANY"), getJobApplications);
 
-//router.patch("/:id", requireAuth, requireRole("COMPANY"));
+router.patch("/:id", requireAuth, requireRole("COMPANY"), updateApplication);
 
 module.exports = router;
